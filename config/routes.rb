8097d0cfa7f06ee8f1b6e3694d8f692c1d81ec7d
qiuxue <1309571292@qq.com>
2022-12-14 20:41:32 +0800

    创建路由

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,15 @@ Rails.application.routes.draw do
   # root "articles#index"
   get '/users/:id', to: 'users#show'
   post 'users/', to: 'users#create'
+
+  namespace :api do
+    namespace :v1 do
+     # /api/v1
+     resources :validation_codes, only: [:create]
+     resource :session, only: [:create, :destroy]
+     resource :me, only: [:show]
+     resources :items
+     resources :tags
+    end
+ end
 end
