133af5b91d5bad83196453cbe92e94c707f6de16
qiuxue <1309571292@qq.com>
2022-12-28 22:23:19 +0800

    feat: items根据user_id和时间范围查询

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-    items = Item.page params[:page]
+    current_user_id = request.env['current_user_id']
+    return head :unauthorized unless current_user_id
+    # 根据user_id和时间范围查询
+    items = Item.where({user_id: current_user_id})
+      .where({created_at: params[:created_after]..params[:created_before]})
     render json: {resources: items, pagination: {
       page: params[:page],
       per_page: 10,
