1100df4558bc8ebe2493660a92e32a87465ff772
qiuxue <1309571292@qq.com>
2022-12-28 22:49:30 +0800

    feat: 处理JWT过期

--- lib/tasks/auto_jwt.rb
+++ lib/tasks/auto_jwt.rb
@@ -4,11 +4,19 @@ class AutoJwt
   end
 
   def call(env)
+    # 跳过 以下路径 的检查
+    return @app.call(env) if ['/api/v1/session'].includes? env['PATH_INFO']
     header = env['HTTP_AUTHORIZATION']
     # formatter: Bearer xxx
     jwt = header.split(' ')[1] rescue ''
-    # decode jwt
-    decoded_arr = JWT.decode jwt, Rails.application.credentials.hmac_secret, true, { algorithm: 'HS256'} rescue nil
+    begin
+      # decode jwt
+      decoded_arr = JWT.decode jwt, Rails.application.credentials.hmac_secret, true, { algorithm: 'HS256'} rescue nil
+    rescue JWT::ExpiredSignature
+      return [401, {}, [JSON.generate({reason: 'Token expired'})]]
+    rescue
+      return [401, {}, [JSON.generate({reason: 'Invalid token'})]]
+    end
     env['current_user_id'] = decoded_arr[0]['user_id'] rescue nil
     @status, @headers, @response = @app.call(env)
     [@status, @headers, @response]
