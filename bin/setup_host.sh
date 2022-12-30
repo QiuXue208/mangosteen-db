6864214c69589ba733712104fbd26775a69c2456
qiuxue <1309571292@qq.com>
2022-12-30 19:20:02 +0800

    feat: 部署到宿主机

--- bin/setup_host.sh
+++ bin/setup_host.sh
@@ -1,14 +1,16 @@
 DB_PASSWORD=123456
-container_name=mangosteen-prod
+container_name=mangosteen-prod-1
 
 version=$(cat mangosteen_deploy/version)
 
 echo 'docker build ...'
 docker build mangosteen_deploy -t mangosteen:$version
-if [ "$(docker ps -aq -f name=^mangosteen-prod$)" ]; then
+if [ "$(docker ps -aq -f name=^mangosteen-prod-1$)" ]; then
   echo 'docker rm ...'
   docker rm -f $container_name
 fi
 echo 'docker run ...'
 docker run -e DB_HOST=$DB_HOST -e RAILS_MASTER_KEY=$RAILS_MASTER_KEY -d -p 3000:3000 --network=network1 -e DB_PASSWORD=$DB_PASSWORD --name=$container_name mangosteen:$version
+# 创建数据库、创建表
+docker exec -it $container_name bin/rails db:create db:migrate
 echo 'DONE!'
--- config/host.Dockerfile
+++ config/host.Dockerfile
@@ -4,11 +4,9 @@ ENV RAILS_ENV production
 RUN mkdir /mangosteen
 RUN bundle config mirror.https://rubygems.org https://gems.ruby-china.com
 WORKDIR /mangosteen
-ADD Gemfile /mangosteen
-ADD Gemfile.lock /mangosteen
-ADD vendor/cache /mangosteen/vendor/cache
-RUN bundle config set --local without 'development test'
-RUN bundle install --local
-
 ADD mangosteen-*.tar.gz ./
+
+RUN bundle config set without 'development test'
+RUN bundle install
+
 ENTRYPOINT bundle exec puma
