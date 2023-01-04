1108a2ec25310ca52f5342e7174724c8700dd270
qiuxue <1309571292@qq.com>
2023-01-04 09:41:39 +0800

    fix: 登录接口语法错误

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::SessionsController < ApplicationController
     # 存在账号code，且used_at为空时才可以登录
     canSignin = ValidationCode.exists?(email: params[:email], code: params[:code], used_at: nil)
     return render status: 401 unless canSignin
-    user = User.find_or_create_by_email(params[:email])
+    user = User.find_or_create_by email: params[:email]
     render status: 200, json: { jwt: user.generate_jwt, }
   end
 end
